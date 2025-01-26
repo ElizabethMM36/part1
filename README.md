41ded4026d530d5fdd0ef7af16900de019dd152a
ElizabethMM36 <elizabethmathew10d@gmail.com>
2025-01-26 21:12:30 +0530

    App.jsx

--- README.md
+++
@@ -1,3 +0,0 @@
-# part1
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ElizabethMM36/part1)
--- introdemo/src/main.jsx
+++ introdemo/src/main.jsx
@@ -1,5 +1,5 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Importing the default export
 
-import App from './App'
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
